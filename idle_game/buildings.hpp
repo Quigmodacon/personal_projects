20c1e30d9f75528d0cdf7f3a9bd80497b01c647c
Jonathan Hansen <j.hansen.oh@gmail.com>
2019-09-19 11:18:04 -0400

    Adding Files.

---
+++ idle_game/buildings.hpp
@@ -0,0 +1,13 @@
+#ifndef BUILDINGS_HPP_
+#define BUILDINGS_HPP_
+
+#include "IDLE.hpp"
+
+factory employee("Employee", 1, 1, 2);
+factory shoes("Shoes", 4, 0, 8);
+factory coffee("Coffee", 8, 0, 16);
+factory tools("Tools", 16, 0, 32);
+
+std::vector<factory> buildings = {employee, shoes, coffee, tools};
+
+#endif
