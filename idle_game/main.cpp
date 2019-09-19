31e3f3367b45bae26ace55f2080929a680d22bb6
Jonathan Hansen <j.hansen.oh@gmail.com>
2019-09-19 16:06:20 -0400

    set up vscode, changed Main
    Fixed main so it works properly now.
    Additionally finished setting up vs code to work with buildingg and running

--- idle_game/main.cpp
+++ idle_game/main.cpp
@@ -12,21 +12,21 @@ int main() { // Main game controller
             for (auto it = buildings.begin(); it < buildings.end(); it++)
                 money += it->payDay();
             print(buildings, money);
-            _getch() >> menuOption;
+            menuOption = _getch();
         } while(menuOption != '1' && menuOption != '2' 
             && menuOption != '3' && menuOption != '4');
         switch(menuOption) {
-        case '1': if (buildings[1].check(money))
-            money -= buildings[1].purchase();
+        case '1': if (buildings[0].check(money))
+                money -= buildings[0].purchase();
             break;
-        case '2': if (buildings[2].check(money))
-            money -= buildings[2].purchase();
+        case '2': if (buildings[1].check(money))
+                money -= buildings[1].purchase();
             break;
-        case '3': if (buildings[3].check(money))
-            money -= buildings[3].purchase();
+        case '3': if (buildings[2].check(money))
+                money -= buildings[2].purchase();
             break;
-        case '4': if (buildings[4].check(money))
-            money -= buildings[4].purchase();
+        case '4': if (buildings[3].check(money))
+                money -= buildings[3].purchase();
             break;
         }
     }
