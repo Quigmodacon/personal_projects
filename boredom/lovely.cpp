3d1c57a683f1a5e41c32606ba858fe4f4ed6733b
Jonathan Hansen <j.hansen.oh@gmail.com>
2019-10-18 14:24:43 -0400

    updating calc

--- boredom/lovely.cpp
+++ boredom/lovely.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
 
 
-int main() {
-	
+int main(int argc, char *argv[]) {
+    // cmd line argument fail check
+    if (argc > 2) {
+        std::cout << "Too many arguments!!!!\n";
+        return 1;
+    }
+
+    // Main Body
+    int count;
+    if (count = *argv[1] - '0') {
+        int i = 100;
+        do {
+            std::cout << count;
+            while (i > 0) 
+                i--;
+            i = 100;
+            count--;
+        } while (count > 0);
+    }
 }
